aeb1f2ff40c7d11cf8b7085054faf634bb25b988
Novak <novaldix@gmail.com>
2023-06-20 10:29:44 +0300

    19_5_2 added solution and description

--- main.cpp
+++ main.cpp
@@ -16,6 +16,15 @@ bool isIncludes(const char &item, const std::string &range) {
                        [&item](const char &c) { return c == item; });
 }
 
+std::string getJoinRange(const std::string &range) {
+    std::string joinRange;
+    for (int i = 0; i < range.size(); ++i) {
+        joinRange += range[i];
+        joinRange += (i != range.size() - 1) ? ',' : '.';
+    }
+
+    return joinRange;
+}
 
 template<typename T> T getUserChar(std::string const &range) {
     T input;
@@ -26,7 +35,7 @@ template<typename T> T getUserChar(std::string const &range) {
         std::cin >> input;
 
         proceed = isIncludes(input, range);
-        if (!proceed) printf(warning, input, range.c_str());
+        if (!proceed) printf(warning, input, getJoinRange(range).c_str());
 
         // Сбрасываем коматозное состояние cin и очищаем потом ввода
         std::cin.clear();
@@ -38,9 +47,9 @@ template<typename T> T getUserChar(std::string const &range) {
 
 int getTypeOfPath() {
     cout << "--------------- INFO --------------\n";
-    cout << "Выберите месторасположение файла:\n";
-    cout << "0 - если он текстовый и в той же директории,\n";
-    cout << "1 - для любого другого файла или другой директорий. Например J:\\C\\19_5_2\\test1.txt\n";
+    cout << "Выберите месторасположение файла (должен быть текстовым):\n";
+    cout << "0 - если в той же директории,\n";
+    cout << "1 - для другой директорий. Например J:\\C\\19_5_2\\test1\n";
     cout << "Итак, выберите 0 или 1: ";
     return (getUserChar<char>("01") - '0');
 }
@@ -59,53 +68,53 @@ bool isFileExist(const char* fileName) {
     return isExist;
 }
 
-void readFile(char* pathToFile) {
+void readFile(const char* fileName) {
     long long bufferSize = 20;
     char* buffer = new char[bufferSize + 1];
     buffer[bufferSize] = 0;
 
-    cout << "Файл найден. Открываю..." << endl;
-    cout << "------------------------" << endl;
+    cout << "Файл найден. Открываю...\n------------------------" << endl;
 
     std::ifstream fileReader;
-    fileReader.open(pathToFile, std::ios::binary);
+    fileReader.open(fileName, std::ios::binary);
     while (!fileReader.eof()) {
         fileReader.read(buffer, bufferSize);
         cout << buffer;
     }
+
+    fileReader.close();
 }
 
 int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    bool continueNext = false;
-    vector<string> ROUTES = { R"(..\%s.txt)", R"(%s)" };
+    bool continueWithSuccess = false;
+    vector<string> ROUTES_INFO = { "название файла", "полный путь и название файла" };
+    vector<string> ROUTES = { R"(..\%s.txt)", R"(%s.txt)" };
 
-    while (!continueNext) {
+    while (!continueWithSuccess) {
         char userInput[128];
         char pathToFile[128];
         int typeOfPath = getTypeOfPath();
 
-        const char* info = typeOfPath
-                           ? "Введите полный путь, название файла и его расширение: "
-                           : "Введите название файла (без расширения): ";
-        cout << info;
+        printf("Введите %s (без расширения): ", ROUTES_INFO[typeOfPath].c_str());
         string pathCurrent = ROUTES[typeOfPath];
 
         cin.getline(userInput, sizeof(userInput), '\n');
         sprintf(pathToFile, pathCurrent.c_str(), userInput);
-        std::cout << "Полный путь до файла: " << pathToFile << std::endl;
+        cout << "Полный путь до файла: " << pathToFile << endl;
 
         if (isFileExist(pathToFile)) {
             readFile(pathToFile);
-            continueNext = true;
+            continueWithSuccess = true;
         } else {
             std::cout << "Неверно. Указанного файла не обнаружено" << std::endl;
             std::cout << "Вы хотите повторить (y/n)?: ";
             if (isIncludes(getUserChar<char>("YyNn"), "Nn")) {
-                continueNext = true;
+                continueWithSuccess = true;
             }
+            system("cls");
         }
     }
 
